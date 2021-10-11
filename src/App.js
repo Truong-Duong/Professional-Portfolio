9cd9dbb65f787c16c9789cb881591142f560ad48
Truong Duong <truong.duong1908@gmail.com>
2021-10-10 23:46:26 -0500

    add src file

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import Header from './components/header';
+import Content from './components/content';
+import Footer from './components/footer';
+
+function App() {
+  // State that stores the current page as a string
+  const [page, setPage] = useState('Home');
+
+  return (
+    <>
+      {/*
+        I'm passing the setPage function to Header
+        because the Header component contains the
+        App's navmenu
+      */}
+      <Header
+        setPage={setPage}
+        page={page}
+      />
+      {/*
+        The Content component needs to be aware of
+        which page the user wants to be on, so that
+        it renders the requested content
+      */}
+      <Content
+        page={page}
+      />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
